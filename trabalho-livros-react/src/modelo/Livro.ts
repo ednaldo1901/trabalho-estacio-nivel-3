eadcbe05b2cc81fe9c68aeaadb64bcbec6170c29
ednaldo1901 <144927754+ednaldo1901@users.noreply.github.com>
2024-08-09 15:44:18 -0300

    editora e livros

---
+++ trabalho-livros-react/src/modelo/Livro.ts
@@ -0,0 +1,12 @@
+export class Livro {
+    constructor(
+        public codigo: number,
+        public codEditora: number,
+        public titulo: string,
+        public resumo: string,
+        public autores: string[]
+    ) {}
+}
+
+
+ 
